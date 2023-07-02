05887d302fa5a32d26cbd89f0e07ef863b2ceaf4
alexis5999 <alexis5999@gmail.com>
2023-07-01 20:53:02 -0500

    ms-admin-api-gateway

---
+++ ms-admin-api-gateway/src/test/java/com/alcale/solutions/MsAdminApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcale.solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAdminApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
